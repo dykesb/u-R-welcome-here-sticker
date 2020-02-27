e108d38ed7cb8b43f657ba4606517520eb908700
B Dykes <dykesb@gvsu.edu>
2020-02-27 18:13:36 -0500

    u-R

--- you-R-welcome-here.R
+++ you-R-welcome-here.R
@@ -7,17 +7,15 @@ library(hexSticker)
 Rainbow_logo <- image_read(paste0(here("Rainbow-logo.png")))
 Rainbow_logo <- image_crop(Rainbow_logo, "724x561+0+563")
 
-white_left <- image_blank(100,700)
-floor_image <- image_blank(724,139)
-shifted_Rainbow_logo <- image_append(c(Rainbow_logo, floor_image), stack = TRUE)
-white_right <- image_blank(1800,700)
+blank_left <- image_blank(200,560)
+blank_right <- image_blank(4000,560)
 
-left_combined <- image_annotate(image_append(c(white_image,shifted_Rainbow_logo)), 
-                                "you", size = 280, font = "sans",
-                                color = "white", location = "+50+304")
+(left_combined <- image_annotate(image_append(c(blank_left,Rainbow_logo)), 
+                                "u", size = 555, font = "sans",
+                                color = "white", location = "+50+46"))
 
-(welcome <- image_annotate(image_append(c(left_combined, white_right)), 
-                          "welcome here", size = 280, font = "sans",
-                          color = "white", location = "+1050+304"))
+(welcome <- image_annotate(image_append(c(left_combined, blank_right)), 
+                          "welcome here", size = 555, font = "sans",
+                          color = "white", location = "+1000+46"))
 
-image_write(welcome, path = paste0(here("you-R-welcome-here.png")))
+image_write(welcome, path = paste0(here("u-R-welcome-here.png")))
